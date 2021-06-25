d72ad53f8145100dfcd5b65fce5e53b1dea90696
AnuPoudyal <79480470+AnuPoudyal@users.noreply.github.com>
2021-06-25 09:11:08 -0400

    committed

---
+++ Lab2/ques.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer();
+const path = require("path");
+
+server.on("request", (req, res) => {
+  if (req.method === "GET" && req.url ==='/') {
+    res.write("I'm learning NodeJS");
+  } else if (req.method === "POST" && req.url ==='/') {
+    const writable = fs.createWriteStream(path.join(__dirname, "/text.txt"));
+    writable.write("Practicing fs");
+  }
+  res.end();
+});
+
+server.listen(3000, () => {
+  console.log("server started on port 3000");
+});
+
+
+
+
+
